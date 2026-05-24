8b9f98eacf851cd31a7ba9693e0ceff5694c2153
Raj Beladiya <rajbeladiya59@gmail.com>
2026-05-24 11:58:12 +0530

    fixed: router issue

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LayoutWrapper />} />
+        <Route path="*" element={<LayoutWrapper />} />
       </Routes>
     </>
   );
